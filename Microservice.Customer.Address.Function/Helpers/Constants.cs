c5a88c4e54b527aeb0d8a8db69bf6ffd23343962
John Miller <john.miller@unionstreet.co.uk>
2024-09-08 15:55:43 +0100

    Access service bus with managed identity

--- Microservice.Customer.Address.Function/Helpers/Constants.cs
+++ Microservice.Customer.Address.Function/Helpers/Constants.cs
@@ -4,7 +4,7 @@ public class Constants
 {
     public const string DatabaseConnectionString = "SQLAZURECONNSTR_CUSTOMER_ADDRESS";
 
-    public const string AzureServiceBusConnection = "AZURE_SERVICE_BUS_CONNECTION";
+    public const string AzureServiceBusConnection = "ServiceBusConnection";
     public const string AzureServiceBusQueueRegisteredUserCustomerAddress = "AZURE_SERVICE_BUS_QUEUE_CUSTOMER_ADDRESS";
 
     public const string FailureReasonValidation = "Validation Errors.";
